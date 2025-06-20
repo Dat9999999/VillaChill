52d470265b3538f762bd55e9b7fe243d4b83e7e7
Dat9999999 <huynhtandat184@gmail.com>
2025-06-20 16:32:14 +0700

    implement IVillaRepository.cs

--- ReservationApp.Infrastructure/Repositories/VillaRepository.cs
+++ ReservationApp.Infrastructure/Repositories/VillaRepository.cs
@@ -1,39 +1,72 @@
 using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
 using ReservationApp.Application.Common.Interfaces;
 using ReservationApp.Domain.Entities;
+using ReservationApp.Infrastructure.Data;
 
 namespace ReservationApp.Infrastructure.Repositories;
 
 public class VillaRepository: IVillaRepository
 {
-    public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, Strings? includeProperty = null)
+    private  readonly ApplicationDbContext _context;
+    public VillaRepository(ApplicationDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+    public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
+    {
+        IQueryable<Villa> query = _context.Set<Villa>();
+        if (filter is not null)
+        {
+            query = query.Where(filter);
+        }
+
+        if (!string.IsNullOrEmpty(includeProperties))
+        {
+            foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+        }
+        return query.ToList();
     }
 
-    public IEnumerable<Villa> Get(Expression<Func<Villa, bool>> filter, Strings? includeProperty = null)
+    public Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
     {
-        throw new NotImplementedException();
+        IQueryable<Villa> query = _context.Set<Villa>();
+        if (filter is not null)
+        {
+            query = query.Where(filter);
+        }
+
+        if (!string.IsNullOrEmpty(includeProperties))
+        {
+            foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+        }
+        return query.FirstOrDefault();
     }
 
     public void Add(Villa villa)
     {
-        throw new NotImplementedException();
+        _context.Villas.Add(villa);
     }
 
     public void Update(Villa villa)
     {
-        throw new NotImplementedException();
+        _context.Villas.Update(villa);
     }
 
     public void Delete(Villa villa)
     {
-        throw new NotImplementedException();
+        _context.Villas.Remove(villa);
     }
 
     public void Save()
     {
-        throw new NotImplementedException();
+        _context.SaveChanges();   
     }
 }
