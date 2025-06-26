d1d83ee17feac58d840293a9bcf545d118f3664d
Dat9999999 <huynhtandat184@gmail.com>
2025-06-26 11:18:36 +0700

    Fix booking repo & Application repo user bug

--- ReservationApp.Application/Common/Interfaces/IUnitOfWork.cs
+++ ReservationApp.Application/Common/Interfaces/IUnitOfWork.cs
@@ -5,5 +5,7 @@ public interface IUnitOfWork
     public IVillaRepository Villas { get; }
     public IVillaNumberRepository VillaNumbers { get; }
     public IAmenityRepository Amenities { get; }
+    public IBookingRepository Bookings { get; }
+    public IApplicationUserRepository ApplicationUsers { get; }
     void Save();
 }
