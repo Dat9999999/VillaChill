679ac3dccbb0ee3e354f94d1814179b03b0aef25
Dat9999999 <huynhtandat184@gmail.com>
2025-07-08 10:57:27 +0700

    Add input for city at Home view

--- ReservationApp.Web/obj/Debug/net9.0/ReservationApp.Web.AssemblyInfo.cs
+++ ReservationApp.Web/obj/Debug/net9.0/ReservationApp.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservationApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94d40cee0177a50e0ef3ae40b47983b8ecc9413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be69b9f759879653db107b553e8c591abdd5c4e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservationApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservationApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
