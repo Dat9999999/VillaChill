a45fd19663e124bc66581e624b9330f29983ac16
Dat9999999 <huynhtandat184@gmail.com>
2025-07-21 09:50:47 +0700

    create signalr-dashboard.js to make dashboard realtime

---
+++ ReservationApp.Web/wwwroot/js/dashboard/signalr-dashboard.js
@@ -0,0 +1,18 @@
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/dashboardHub")
+    .build();
+
+connection.on("UserRegistered", function (data) {
+    console.log("📡 UserRegistered received:", data);
+    loadUserRadialChart();
+});
+
+connection.start().then(function () {
+    console.log("✅ SignalR Connected.");
+}).catch(function (err) {
+    console.error("❌ SignalR Connection Failed:", err.toString());
+});
+
+connection.onclose(function (err) {
+    console.error("⚠️ SignalR Connection closed:", err);
+});
